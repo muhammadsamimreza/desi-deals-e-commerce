4dba289dad6930316133a29c8b3da9531dcafc9e
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-08-25 23:56:11 +0600

    Delegation Implement of all product Card

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-const allBtns = document.getElementsByClassName("add-btn");
-for (const button of allBtns) {
-  button.addEventListener("click", () => {
+// Delegation System
+
+document.getElementById("all-product-box").addEventListener("click", (e) => {
+  if (e.target.className.includes("add-btn")) {
+    const button = e.target;
     const productImage =
       button.parentNode.parentNode.children[0].children[0].src;
-     
 
-    const productTitle = button.parentNode.parentNode.children[1].children[1].innerText;
-    
+    const productTitle =
+      button.parentNode.parentNode.children[1].children[1].innerText;
+
     const productPrice =
-    button.parentNode.parentNode.children[1].children[2].children[0].innerText;
+      button.parentNode.parentNode.children[1].children[2].children[0]
+        .innerText;
     // console.log(productPrice)
 
     const showSection = document.getElementById("Purchase-list");
@@ -32,17 +35,77 @@ for (const button of allBtns) {
     `;
     showSection.appendChild(div);
 
-    const totalPrice = parseInt(document.getElementById('totalPrice').innerText);
+    const totalPrice = parseInt(
+      document.getElementById("totalPrice").innerText
+    );
 
     const currentBalance = Number(productPrice) + totalPrice;
 
-    document.getElementById('totalPrice').innerText = currentBalance;
+    document.getElementById("totalPrice").innerText = currentBalance;
 
-    const quantity = parseInt(document.getElementById('quantity').innerText);
+    const quantity = parseInt(document.getElementById("quantity").innerText);
 
     const totalQuantity = quantity + 1;
-    document.getElementById('quantity').innerText = totalQuantity;
+    document.getElementById("quantity").innerText = totalQuantity;
+  }
+});
 
-  });
-  // document.getElementById('Purchase-list').innerText ='';
-}
+// Traverse System
+// const allBtns = document.getElementsByClassName("add-btn");
+
+// for (const button of allBtns) {
+//   button.addEventListener("click", () => {
+//     const productImage =
+//       button.parentNode.parentNode.children[0].children[0].src;
+
+//     const productTitle =
+//       button.parentNode.parentNode.children[1].children[1].innerText;
+
+//     const productPrice =
+//       button.parentNode.parentNode.children[1].children[2].children[0]
+//         .innerText;
+//     // console.log(productPrice)
+
+//     const showSection = document.getElementById("Purchase-list");
+//     const div = document.createElement("div");
+//     div.innerHTML = `
+//         <div class="bg-gray-100 shadow-md border-1 border-gray-300 m-3 rounded-xl flex justify-between items-center gap-14">
+//                 <figure class="rounded-2xl">
+//                   <img
+//                     src="${productImage}"
+//                     alt=""
+//                     class="rounded-xl mx-auto w-14 pl-2"
+//                   />
+//                 </figure>
+//                 <div class="card-body">
+//                   <h2 class="card-title text-base font-semibold">${productTitle}</h2>
+//                   <p class="text-base text-gray-500 font-semibold">
+//                     <span>${productPrice}</span> Tk
+//                   </p>
+//                 </div>
+//               </div>
+//     `;
+//     showSection.appendChild(div);
+
+//     const totalPrice = parseInt(
+//       document.getElementById("totalPrice").innerText
+//     );
+
+//     const currentBalance = Number(productPrice) + totalPrice;
+
+//     document.getElementById("totalPrice").innerText = currentBalance;
+
+//     const quantity = parseInt(document.getElementById("quantity").innerText);
+
+//     const totalQuantity = quantity + 1;
+//     document.getElementById("quantity").innerText = totalQuantity;
+//   });
+// }
+
+// Clear Purchase Product ///
+
+document.getElementById("clear-btn").addEventListener("click", () => {
+  document.getElementById("Purchase-list").innerHTML = "";
+  document.getElementById("quantity").innerText = 0;
+  document.getElementById("totalPrice").innerText = 0;
+});
